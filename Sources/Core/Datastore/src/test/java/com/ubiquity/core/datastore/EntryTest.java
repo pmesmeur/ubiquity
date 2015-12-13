eb501c31589ac144d60a376a82c6c0b8b997aebd
pmesmeur <philippe.mesmeur@misys.com>
2015-12-13 04:09:10 +0100

    enhance unit tests of class Entry

--- Sources/Core/Datastore/src/test/java/com/ubiquity/core/datastore/EntryTest.java
+++ Sources/Core/Datastore/src/test/java/com/ubiquity/core/datastore/EntryTest.java
@@ -14,7 +14,16 @@ public class EntryTest {
 
     @Test(expected = AssertionError.class)
     public void testCreatingEntryWithNullDataDefinition() {
-        new Entry(null, createBasicEntryValues());
+        Map<String, Object> entryValues = new HashMap<String, Object>();
+
+        entryValues.put("Field1", new Boolean(true));
+        entryValues.put("Field2", new Double(1.));
+        entryValues.put("Field3", new Character('c'));
+        entryValues.put("Field4", new Integer(27));
+        entryValues.put("Field5", new String("Ubiquity"));
+        entryValues.put("Field6", new HashMap<String, String>());
+
+        new Entry(null, entryValues);
     }
 
 
@@ -26,7 +35,16 @@ public class EntryTest {
 
     @Test
     public void testCreatingBasicEntry() {
-        new Entry(createBasicEntryDataDefinition(), createBasicEntryValues());
+        Map<String, Object> entryValues = new HashMap<String, Object>();
+
+        entryValues.put("Field1", new Boolean(true));
+        entryValues.put("Field2", new Double(1.));
+        entryValues.put("Field3", new Character('c'));
+        entryValues.put("Field4", new Integer(27));
+        entryValues.put("Field5", new String("Ubiquity"));
+        entryValues.put("Field6", new HashMap<String, String>());
+
+        new Entry(createBasicEntryDataDefinition(), entryValues);
     }
 
 
@@ -53,20 +71,6 @@ public class EntryTest {
     }
 
 
-    private Map<String, Object> createBasicEntryValues() {
-        Map<String, Object> entryValues = new HashMap<String, Object>();
-
-        entryValues.put("Field1", new Boolean(true));
-        entryValues.put("Field2", new Double(1.));
-        entryValues.put("Field3", new Character('c'));
-        entryValues.put("Field4", new Integer(27));
-        entryValues.put("Field5", new String("Ubiquity"));
-        entryValues.put("Field6", new HashMap<String, String>());
-
-        return entryValues;
-    }
-
-
     private IFieldDefinition fieldDefinition(final String name,
                                              final Type type,
                                              final boolean isIndexed,
@@ -89,4 +93,35 @@ public class EntryTest {
             }
         };
     }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreatingEntryWithMissingMandatoryField() {
+        Map<String, Object> entryValues = new HashMap<String, Object>();
+
+        entryValues.put("Field1", new Boolean(true));
+        entryValues.put("Field2", new Double(1.));
+        entryValues.put("Field4", new Integer(27));
+        entryValues.put("Field5", new String("Ubiquity"));
+        entryValues.put("Field6", new HashMap<String, String>());
+
+        new Entry(createBasicEntryDataDefinition(), entryValues);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreatingEntryWithBadTypedField() {
+        Map<String, Object> entryValues = new HashMap<String, Object>();
+
+        entryValues.put("Field1", new Boolean(true));
+        entryValues.put("Field2", new Double(1.));
+        entryValues.put("Field3", new Integer(666));
+        entryValues.put("Field4", new Integer(27));
+        entryValues.put("Field5", new String("Ubiquity"));
+        entryValues.put("Field6", new HashMap<String, String>());
+
+        new Entry(createBasicEntryDataDefinition(), entryValues);
+    }
+
+
 }
