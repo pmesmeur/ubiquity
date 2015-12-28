134b1165216d7b7560e2300fe4395bb1b2fda94f
pmesmeur <philippe.mesmeur@gmail.com>
2015-12-28 13:18:06 +0100

    Start working on DataParser

--- Tests/BasicTest/src/main/java/com/ubiquity/tests/basic/DataParser.java
+++ Tests/BasicTest/src/main/java/com/ubiquity/tests/basic/DataParser.java
@@ -1,16 +1,46 @@
 package com.ubiquity.tests.basic;
 
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Map;
 
 public class DataParser {
 
-    public void parse(String fileName, IDataInsertor iDataInsertor) {
-
+    public DataParser() {
+        init();
     }
+
+    private void init() {
+    }
+
+    public void parse(String fileName, IDataInsertor dataInsertor) {
+        init();
+        TextFileReader textFileReader = new TextFileReader();
+
+        try {
+            parseImpl(fileName, textFileReader, dataInsertor);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseImpl(String fileName, TextFileReader textFileReader, IDataInsertor dataInsertor) throws IOException {
+        textFileReader.open(fileName);
+        textFileReader.read(new LineProcessor());
+    }
+
 
     public interface IDataInsertor {
         void insert(String dataShelf, String dataIdentifier, Map<String, Object> dataFields);
+    }
+
+    private class LineProcessor implements ILineProcessor {
+        public void processLine(String line) {
+
+        }
     }
 
 }
